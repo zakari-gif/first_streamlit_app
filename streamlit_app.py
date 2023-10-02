7434208d3507dfb0efb343234b83feb0f9c3d163
Zaky <56865805+zakari-gif@users.noreply.github.com>
2023-10-02 11:35:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, spinach & Rocket Smoothis')
 streamlit.text('🥑🍞 Hard-Boiled Free- Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
