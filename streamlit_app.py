a9adee72815e55b28fc4694db8acdc80c28f2b57
Zaky <56865805+zakari-gif@users.noreply.github.com>
2023-10-02 11:29:09 +0200

    Update streamlit_app.py
    script_one_update2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner zakari")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothis')
+streamlit.text('Hard-Boiled Free- Range Egg')
